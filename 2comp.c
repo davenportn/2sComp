dba9465e4af734fc73f245254a54df1e35516c08
davenportn <ntldvnprt@gmail.com>
2015-10-04 13:31:56 -0400

    Adding basic class

---
+++ 2comp.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+int main(int argc, char *argv[])
+{
+	//Check for correct number of arguements 	
+	if (argc > 2 || argc == 1)
+	{
+		printf("Please use the appropriate number of arguements\n");
+		return 1;
+	}
+
+	printf("Input: %s\n", argv[1]);
+
+
+	if ((strlen(argv[1]) % 4) != 0 )
+	{
+		//Not in standard byte, will address
+		printf("Not in byte form\n");
+		return 1;
+	}
+	else {
+		//For now, just end. Then plan what will happen
+		return 0;
+	}	
+}
+
